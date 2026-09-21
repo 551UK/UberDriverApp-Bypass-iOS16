fef56ba22c4c656d601245b0d00d12771458b733
551 <wdf@hotmail.co.uk>
2026-09-21 22:58:12 +0100

    Fix v0.3.1 regression harness after removing runtime hooks

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -10,7 +10,7 @@ constants = source[
 ]
 functions = source[
     source.index("static NSString *UBNormalizedKey"):
-    source.index("static BOOL UBSelectorLooksLikeApplicabilityCheck")
+    source.index("static BOOL UBIsUberURL")
 ]
 
 harness = r"""
